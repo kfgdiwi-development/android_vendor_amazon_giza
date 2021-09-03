8931a9674265b5257b75f5068827ae9087238e57
Roger Ortiz <rogerortizleal@gmail.com>
2021-09-03 14:36:48 +0200

    giza: set up vendor for lineage-15.1
    Signed-off-by: Roger Ortiz <rogerortizleal@gmail.com>

--- Android.mk
+++ Android.mk
@@ -40,4 +40,26 @@ LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libcam.client
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_OWNER := amazon
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MULTILIB := both
+LOCAL_SRC_FILES_64 := proprietary/lib64/libcam.client.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libcam.client.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libcam_utils
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_OWNER := amazon
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MULTILIB := both
+LOCAL_SRC_FILES_64 := proprietary/lib64/libcam_utils.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libcam_utils.so
+include $(BUILD_PREBUILT)
+
 endif
